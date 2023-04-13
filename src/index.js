cce4901160f93c5906c40cc112136a07a26772b5
Sadiyya-k <125921130+Sadiyya-k@users.noreply.github.com>
2023-04-13 21:40:09 +0100

    Updated city time/date

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ function updateTime() {
   let istanbulElement = document.querySelector("#istanbul");
   let istanbulDateElement = istanbulElement.querySelector(".date");
   let istanbulTimeElement = istanbulElement.querySelector(".time");
-  let istanbulTime = moment().tz("Europe/London");
+  let istanbulTime = moment().tz("Europe/Istanbul");
 
-  istanbulDateElement.innerHtml = istanbulTime.format("MMMM Do YYYY");
+  istanbulDateElement.innerHTML = istanbulTime.format("MMMM Do YYYY");
   istanbulTimeElement.innerHTML = istanbulTime.format(
     "H:mm:ss [<small>]A[</small>]"
   );
@@ -12,21 +12,21 @@ function updateTime() {
   let bangkokElement = document.querySelector("#bangkok");
   let bangkokDateElement = bangkokElement.querySelector(".date");
   let bangkokTimeElement = bangkokElement.querySelector(".time");
-  let bangkokTime = moment().tz();
+  let bangkokTime = moment().tz("Asia / Bangkok");
 
   bangkokDateElement.innerHTML = bangkokTime.format("MMMM Do YYYY");
   bangkokTimeElement.innerHTML = bangkokTime.format(
     "H:mm:ss [<small>]A[</small>]"
   );
 
-  let romeElement = document.querySelector("#bangkok");
+  let romeElement = document.querySelector("#rome");
   let romeDateElement = romeElement.querySelector(".date");
   let romeTimeElement = romeElement.querySelector(".time");
-  let romeTime = moment().tz();
+  let romeTime = moment().tz("Europe/Rome");
 
   romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
   romeTimeElement.innerHTML = romeTime.format("H:mm:ss [<small>]A[</small>]");
-
-  updateTime();
-  setInterval(updateTime, 1000);
 }
+
+updateTime();
+setInterval(updateTime, 1000);
