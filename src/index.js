0980f2c701361776d04acaedbf751994dff75cbf
Sadiyya-k <125921130+Sadiyya-k@users.noreply.github.com>
2023-04-14 16:08:36 +0100

    Updated to add selected location

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ function selectedCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+  citiesElement.innerHTML += `
         <div class="city">
           <div>
             <h2>${cityName}</h2>
